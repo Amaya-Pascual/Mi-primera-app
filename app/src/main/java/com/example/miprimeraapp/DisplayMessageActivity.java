3171384af1267d87d890f4d194db0c06d899086d
Amaya-Pascual <apascualv01@educantabria.es>
2020-11-11 12:25:42 +0100

    No pasa los datos

--- app/src/main/java/com/example/miprimeraapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/miprimeraapp/DisplayMessageActivity.java
@@ -4,34 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE ="com.example.miprimeraapp.MESSAGE";
+    Button siguiente;
+    //public static final String EXTRA_MESSAGE ="com.example.miprimeraapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-        //Obtiene el intent que empieza esta actividad y recoge el string
-        Intent intent=getIntent();
-        String message=intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        //captura el layout del TextView de la segunda actividad y pone el string en el
-        TextView textView=findViewById((R.id.textView));
-        textView.setText(message);
+        siguiente=(Button)findViewById(R.id.siguientepantalla);
+        siguiente.setOnClickListener(new View.OnClickListener() {
 
+            public void sgtepantalla(View v) {
+                Intent siguiente =new Intent(DisplayMessageActivity.this,activity_show_somethig.class);
+                startActivity(siguiente);
+            }
+        });
     }
-    /**Llamado cuando el usuario toca el botón Siguiente pantalla*/
-    public void showSomething(ImageView imageView)
+
+    /**Llamado cuando el usuario toca el botón
+    public void sgtepantalla(View view)
     {
-        Intent intent2;
-        intent2 = new Intent(this, activity_show_somethig.class);
-        ImageView entry = (ImageView) findViewById(R.id.imageView);
-
-        entry.setImageResource(R.drawable.castrourdiales);
-        //String message= editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent2);
-
-    }
+        Intent intent;
+        intent = new Intent(this, activity_show_somethig.class);
+        ImageView imageView;
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.imagen01);
+        startActivity(intent);
+    }*/
 }
